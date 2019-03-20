124cfb5f7f06c772a24afb53b532ded29ab8ee6c
Damian <damianjasinowski@icloud.com>
2019-03-20 19:19:19 +0000

    Comment out if statements and finish first part of choices.

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -11,22 +11,42 @@ import UIKit
 class ViewController: UIViewController {
     
     // Our strings
-    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
-    let answer1a = "I\'ll hop in. Thanks for the help!"
-    let answer1b = "Better ask him if he\'s a murderer first."
+    let story1 = "Your on a bush plane in the co-pilot seat. Your flying to your dad’s house in Canada. Your going fly to your dad’s every summer but today is the first summer you will have to fly to your dad’s. It is the first summer after the divorce your parents had, had. For most of the flight all you can see are forests and a few lakes here and there. You remember that just before you left your mum gave you a hatchet it fitted nicely in your belt and as you looked towards your belt and realise that it is still in your belt. Then you went towards the plane without saying thank you or goodbye. After being on the plane for a couple of hours, the pilot asks you if you want to try fly the plane."
+    let answer1a = "You try fly the plane."
+    let answer1b = "You thankfully dismiss the pilot’s offer."
+
+    let story2 = "The pilot slowly lifts his foot off the rudders and let’s go of the plane’s controls. You take the controls and realize it is surprisingly easy to fly it. You let go of the controls and let the pilot take the controls back. You say thank you to him. You become friends with the pilot."
+    let answer2 = "Continue"
     
+    let story3 = "Even though you dismissed his offer he still lets go off the rudders and the controls "
+    let answer3a = "Leave the controls untouched."
+    let answer3b = "Grab the controls quickly."
     
-    let story2 = "He nods slowly, unphased by the question."
-    let answer2a = "At least he\'s honest. I\'ll climb in."
-    let answer2b = "Wait, I know how to change a tire."
+    let story4 = "The pilot regains control not looking as happy as he was before he left."
+    let answer4 = "Continue"
+
+    let story5 = "You quickly grab the controls. And you accidentally press a control and the plane starts twisting in the air. You loose control and start diving towards a lake. Luckily, the pilot grabs the controls before any serious damage had happened."
+    let answer5a = "Say, ’Why did you do that you could have got us killed!’"
+    let answer5b = "Say, ‘Thank you for believing in me but I’m not really in a good mood’"
     
-    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
-    let answer3a = "I love Elton John! Hand him the cassette tape."
-    let answer3b = "It\'s him or me! You take the knife and stab him."
+    let story6 = "You have become enemies with the pilot"
+    let answer6 = "Continue"
+
     
-    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
-    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
-    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    let story7 = "The pilot brightens up a little."
+    let answer7 = "Continue"
+    
+    let story8 = "As you continue on your flight you smell a horrid odour coming from the pilot and notice that he keeps scratching himself. A few minutes later, the odour keeps getting stronger and the pilot starts to scratch himself more often."
+    let answer8a = "Get your deodorant out of your bag and start spraying him with it and tell him he needs a shower!"
+    let answer8b = "Ignore the smell and pretend you don’t notice it."
+    
+    let story9 = "The pilot is secretly allergic to any deodorant and his face became itchy, bumpy red and can started to blister. The pilot shouts in pain, agony and angriness. He lets go of the controls and are heading towards the ground."
+    let answer9a = "Grab the controls quickly."
+    let answer9b = "Ask the pilot to calm down and grab his controls."
+    
+    let story10 = "The odour kept worsening. Suddenly, a spasm hit the pilot the force of a hammer. The pilot stopped his mouth went rigid. The pilot said “My chest...it’s tearing apart”. He fell on the floor. Your realise it was a heart attack."
+    let answer10a = "Grab the controls over the pilots body."
+    let answer10b = "Take the seat belt off the pilot and drop him to the floor. Then sit on his seat and grab the controls."
     
     
     // UI Elements linked to the storyboard
@@ -58,40 +78,40 @@ class ViewController: UIViewController {
 
 //         TODO Step 4: Write an IF-Statement to update the views
 
-        if sender.tag == 1 && storyIndex == 1 {
-            storyTextView.text = story3
-            topButton.setTitle(answer3a, for: .normal)
-            bottomButton.setTitle(answer3b, for: .normal)
-            storyIndex = 3
-        } else if sender.tag == 2 && storyIndex == 1 {
-            storyTextView.text = story2
-            topButton.setTitle(answer2a, for: .normal)
-            bottomButton.setTitle(answer2b, for: .normal)
-            storyIndex = 2
-        } else if sender.tag == 1 && storyIndex == 3 {
-            storyTextView.text = story6
-            topButton.isHidden = true
-            bottomButton.isHidden = true
-            storyIndex = 6
-        } else if sender.tag == 2 && storyIndex == 3 {
-            storyTextView.text = story5
-            topButton.isHidden = true
-            bottomButton.isHidden = true
-            storyIndex = 5
-        } else if sender.tag == 1 && storyIndex == 2 {
-            storyTextView.text = story3
-            topButton.setTitle(answer3a, for: .normal)
-            bottomButton.setTitle(answer3b, for: .normal)
-            storyIndex = 3
-        } else if sender.tag == 2 && storyIndex == 2 {
-            storyTextView.text = story4
-            topButton.isHidden = true
-            bottomButton.isHidden = true
-            storyIndex = 4
-        }
-        if storyIndex == 6 || storyIndex == 5 || storyIndex == 4 {
-            restartButton.isHidden = false
-        }
+//        if sender.tag == 1 && storyIndex == 1 {
+//            storyTextView.text = story3
+//            topButton.setTitle(answer3a, for: .normal)
+//            bottomButton.setTitle(answer3b, for: .normal)
+//            storyIndex = 3
+//        } else if sender.tag == 2 && storyIndex == 1 {
+//            storyTextView.text = story2
+//            topButton.setTitle(answer2a, for: .normal)
+//            bottomButton.setTitle(answer2b, for: .normal)
+//            storyIndex = 2
+//        } else if sender.tag == 1 && storyIndex == 3 {
+//            storyTextView.text = story6
+//            topButton.isHidden = true
+//            bottomButton.isHidden = true
+//            storyIndex = 6
+//        } else if sender.tag == 2 && storyIndex == 3 {
+//            storyTextView.text = story5
+//            topButton.isHidden = true
+//            bottomButton.isHidden = true
+//            storyIndex = 5
+//        } else if sender.tag == 1 && storyIndex == 2 {
+//            storyTextView.text = story3
+//            topButton.setTitle(answer3a, for: .normal)
+//            bottomButton.setTitle(answer3b, for: .normal)
+//            storyIndex = 3
+//        } else if sender.tag == 2 && storyIndex == 2 {
+//            storyTextView.text = story4
+//            topButton.isHidden = true
+//            bottomButton.isHidden = true
+//            storyIndex = 4
+//        }
+//        if storyIndex == 6 || storyIndex == 5 || storyIndex == 4 {
+//            restartButton.isHidden = false
+//        }
     }
 
         // TODO Step 6: Modify the IF-Statement to complete the story
